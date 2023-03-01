ec552ab7d4ffe31b8915fe32a2afaa6fb17fc51d
Kotaro Kawazoe <kotaro.kawazoe@medpeer.co.jp>
2023-03-01 14:57:41 +0900

    02の回答を追加

--- 03_method/02_define.rb
+++ 03_method/02_define.rb
@@ -2,6 +2,12 @@
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
 
+class A1
+  define_method '//'.to_sym do
+    '//'
+  end
+end
+
 # Q2.
 # 次の動作をする A2 class を実装する
 # - 1. "SmartHR Dev Team"と返すdev_teamメソッドが存在すること
@@ -12,7 +18,45 @@
 # - また、2で定義するメソッドは以下を満たすものとする
 #   - メソッドが定義されるのは同時に生成されるオブジェクトのみで、別のA2インスタンスには（同じ値を含む配列を生成時に渡さない限り）定義されない
 
+class A2
+  def initialize(arr)
+    arr.each do |e|
+      add_singleton_methods(e)
+    end
+  end
+
+  def dev_team
+    "SmartHR Dev Team"
+  end
+
+  def add_singleton_methods(arg)
+    define_singleton_method "hoge_#{arg}".to_sym do |arg|
+      return dev_team if arg.nil?
+      __method__.to_s * arg
+    end
+  end
+end
+
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+
+module OriginalAccessor
+  def self.included(base)
+    base.define_singleton_method :my_attr_accessor do |attribute|
+      base.define_method attribute do
+        @attribute
+      end
+
+      base.define_method "#{attribute}=" do |arg|
+        @attribute = arg
+        if [TrueClass,FalseClass].include? arg.class
+          define_singleton_method "#{attribute}?" do
+            !!arg
+          end
+        end
+      end
+    end
+  end
+end
