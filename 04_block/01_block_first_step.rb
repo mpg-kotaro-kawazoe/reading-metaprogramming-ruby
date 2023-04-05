0326c87554a20ad610950011674141540167ced3
Kotaro Kawazoe <kotaro.kawazoe@medpeer.co.jp>
2023-04-05 13:04:40 +0900

    first_stepの回答を追加

--- 04_block/01_block_first_step.rb
+++ 04_block/01_block_first_step.rb
@@ -3,6 +3,9 @@
 #    実行例: MyMath.new.two_times { 2 } #=> 4
 
 class MyMath
+  def two_times
+    yield * 2
+  end
 end
 
 # 2. AcceptBlockクラスにcallクラスメソッドが予め定義されており、このメソッドがブロックをとるとします。
@@ -12,10 +15,15 @@ end
 
 MY_LAMBDA = -> { 3 }
 
+AcceptBlock.call(&MY_LAMBDA)
+
 # 3. MyBlockクラスにblock_to_procインスタンスメソッドを定義しましょう。block_to_procインスタンスメソッドはブロックを受け取り、
 #    そのブロックをProcオブジェクトにしたものを返します
 
 class MyBlock
+  def block_to_proc(&block)
+    block
+  end
 end
 
 # 4. MyClosureクラスにincrementインスタンスメソッドを定義しましょう。このincrementメソッドは次のように数値を1ずつインクリメントして返します
@@ -32,6 +40,11 @@ end
 #    さらなる制限として、カウンターとして利用する変数はローカル変数を利用してください(これはテストにはないですが頑張ってローカル変数でテストを通るようにしてみてください)
 
 class MyClosure
+  counter = 0
+
+  define_method :increment do
+    counter +=1
+  end
 end
 
 
